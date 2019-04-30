5a707c51da979dd0959e06282f13faaf5a513fbd
Daniel Schneider <daschne8@gmail.com>
2019-04-30 17:19:12 +0000

    Automatically backed up by Learn

--- db/migrate/04_create_title_figures.rb
+++ db/migrate/04_create_title_figures.rb
@@ -1,7 +1,8 @@
 class CreateTitleFigures < ActiveRecord::Migration[5.2]
   def change
     create_table :title_figures do |t|
-
+      t.integer :title_id
+      t.integer :figure_id
     end
   end
 end
