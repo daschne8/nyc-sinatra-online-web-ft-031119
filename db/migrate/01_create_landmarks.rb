752e4d710bce58f17b784477733142f32be46bd6
Daniel Schneider <daschne8@gmail.com>
2019-04-30 16:44:45 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -1,3 +1,10 @@
-class CreateLandmarks < ActiveRecord::Migration
+class CreateLandmarks < ActiveRecord::Migration[5.2]
   raise 'Write CreateLandmarks migration here'
+  def change
+    create_table :landmarks do |t|
+      t.string :name
+    end
+  end
+
+
 end
