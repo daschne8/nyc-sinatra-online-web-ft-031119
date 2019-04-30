e91ece5dc83ff053282f541484d060f775b3f92a
Daniel Schneider <daschne8@gmail.com>
2019-04-30 17:17:27 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -3,6 +3,8 @@ class CreateLandmarks < ActiveRecord::Migration[5.2]
   def change
     create_table :landmarks do |t|
       t.string :name
+      t.integer :figure_id
+      t.datetime :year_completed
     end
   end
 
